d6182f76df5ab11ef2bb4301d5c95b6298aeb94b
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-05-03 07:34:38 +0530

    *,+.-./ functions done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,63 @@ export class AppComponent {
 
   calValue:number=0
   funcT:any = 'NoFunction'
+  calNumber:string='noValue'
+
+  firstNumber:number = 0
+  secondNumber:number =0
+
+  onClickValue(val:string,type:any){
+    if(type == 'number'){
+      this.onNumberClick(val)
+    }else if(type == 'function'){
+      this.onFunctionClick(val)
+    }
+  }
+
+  onNumberClick(val:string){
+    if(this.calNumber != 'noValue'){
+      this.calNumber=this.calNumber + val
+    }else{
+      this.calNumber = val
+    }
+    this.calValue = parseFloat(this.calNumber)
+  }
+
+  onFunctionClick(val:string){
+    if(this.funcT == 'NoFunction'){
+      this.firstNumber = this.calValue
+      this.calValue = 0
+      this.calNumber = 'noValue'
+      this.funcT = val
+    }else if(this.funcT!='NoFunction'){
+      this.secondNumber= this.calValue
+      this.valueCalculate(val)
+        }
+  }
+
+  valueCalculate(val:string){
+    if(this.funcT == '+'){
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignedValue(Total,val)
+    }
+    if(this.funcT == '-'){
+      const Total = this.firstNumber - this.secondNumber
+      this.totalAssignedValue(Total,val)
+    }
+    if(this.funcT == '*'){
+      const Total = this.firstNumber * this.secondNumber
+      this.totalAssignedValue(Total,val)
+    }
+    if(this.funcT == '/'){
+      const Total = this.firstNumber / this.secondNumber
+      this.totalAssignedValue(Total,val)
+    }
+  }
+
+  totalAssignedValue(Total:number,val:string){
+    this.calValue = Total
+    this.firstNumber = Total
+    this.calNumber = 'noValue'
+    this.funcT = val
+  }
 }
